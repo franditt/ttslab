cd35d86d9a1556606b3d00db78186aad500b4999
Daniel van Niekerk <dvniekerk@csir.co.za>
2012-06-01 16:10:49 +0200

    added .gitignore file and init file for the ttslab python package...

---
+++ ttslab/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+    Utilities used by ttslab...
+"""
+
+try:
+    import cPickle as pickle  #Py2
+except ImportError:
+    import pickle
+
+
+def extend(cls, module_name):
+    exec("import " + module_name)
+    funclist = [name for name in eval("dir(%s)" % module_name)
+                if eval("str(getattr(%s, '%s'))" % (module_name, name)).startswith("<function ")]
+    for funcname in funclist:
+        setattr(cls, funcname, eval("getattr(%s, '%s')" % (module_name, funcname)))
+
+def fromfile(fname):
+    with open(fname) as infh:
+        return pickle.load(infh)
+
+def tofile(obj, fname):
+    with open(fname, "w") as outfh:
+        pickle.dump(obj, outfh, protocol=2)
